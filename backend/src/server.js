3415d0f1c39798801fd8ca860c5a10e4f67023b8
thanhtris <thanhtritran8@gmail.com>
2025-07-01 09:36:48 +0700

    startServer func added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,19 @@ import { connectDB } from "./config/db.js";
 
 const app = express();
 
-connectDB();
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
 
 app.get("/", (req, res) => res.send("Hello, World!"));
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
-});
+startServer();
